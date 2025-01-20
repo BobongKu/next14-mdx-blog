8165ee535d149449c72650617aa44bc0f03099bf
DevBO <jhbgjy7@gmail.com>
2025-01-20 10:14:28 +0900

    remove thumbnail

--- src/components/post_list/PostCard.tsx
+++ src/components/post_list/PostCard.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 import { Post } from '@/config/types';
 import { CalendarDays, Clock3 } from 'lucide-react';
 import { TransitionLink } from '../common/TransitionLink';
@@ -11,22 +9,10 @@ interface Props {
 const PostCard = ({ post }: Props) => {
   return (
     <TransitionLink href={post.url}>
-      <li className='z-0 group flex h-full flex-col gap-3 overflow-hidden rounded-md border shadow-md transition hover:shadow-xl dark:border-slate-700 dark:hover:border-white'>
-        <div className='relative aspect-video w-full rounded-t-md border-b'>
-          <Image
-            src={post.thumbnail}
-            alt={`thumbnail for ${post.title}`}
-            sizes='(max-width: 1000px) 50vw, 450px'
-            fill
-            priority
-            style={{
-              objectFit: 'cover',
-            }}
-          />
-        </div>
+      <li className='z-0 group flex h-full flex-col gap-4 overflow-hidden rounded-md border shadow-md transition hover:shadow-xl dark:border-slate-700 dark:hover:border-white'>
         <div className='flex flex-1 flex-col justify-between p-4 pt-1'>
           <div>
-            <div className='text-sm font-medium text-gray-500 lg:text-base'>
+            <div className='text-sm font-medium text-gray-500 lg:text-base pt-2'>
               {post.categoryPublicName}
             </div>
             <h2 className='mt-1 text-lg font-bold sm:text-xl md:text-lg' >
