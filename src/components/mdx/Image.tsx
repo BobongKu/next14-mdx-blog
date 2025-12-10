83afc9ce44551a7c6dfa81bd4e0ff325dfe6960b
DevBO <42234678+BobongKu@users.noreply.github.com>
2025-12-10 14:05:06 +0900

    add img border

--- src/components/mdx/Image.tsx
+++ src/components/mdx/Image.tsx
@@ -6,7 +6,7 @@ interface ImageProps {
 export const Image = ({ src, alt }: ImageProps) => {
   return (
     <>
-      <img src={src} alt={alt} className='mx-auto mb-0 !mt-4 rounded-md' />
+      <img src={src} alt={alt} className='mx-auto mb-0 !mt-4 rounded-md border-2 border-gray-500'/>
       {alt !== '' && (
         <span className='mb-8 block w-full text-center text-sm text-gray-500 dark:text-gray-400'>
           {alt}
